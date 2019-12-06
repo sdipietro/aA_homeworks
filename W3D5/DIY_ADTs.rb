6cf3f183eacebebb627c7083a68cc75db631c8a9
your name here <stevedipietro97@gmail.com>
2019-12-06 10:07:06 -0500

    Added Map class

--- W3D5/DIY_ADTs.rb
+++ W3D5/DIY_ADTs.rb
@@ -52,3 +52,32 @@ p q.dequeue
 p q.peek
 p q
 
+class Map
+    def initialize
+        @map = []
+    end
+    def set(k,v)
+        if map.any? {|sub_arr| sub_arr[0] == k}
+            (0...map.length).each {|i| map[i][1] = v if map[i][0] == k}
+        else
+            map << [k,v]
+        end
+    end
+    def get(k)
+        map.each {|sub_arr| return sub_arr[1] if sub_arr[0] == k}
+    end
+    def delete(k)
+        (0...map.length).each do |i|
+            if map[i][0] == k
+                @map = map[0...i] + map[i+1..-1]
+                return
+            end
+        end
+    end
+    def show
+        @map
+    end
+    private
+    attr_reader :map
+end
+
