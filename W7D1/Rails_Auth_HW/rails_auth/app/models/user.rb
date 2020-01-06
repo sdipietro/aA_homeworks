b0f42ab976fec43d18c4d966f75bed52b767c89c
Stephen DiPietro <stevedipietro97@gmail.com>
2020-01-06 14:58:12 -0500

    Fixed User model

--- W7D1/Rails_Auth_HW/rails_auth/app/models/user.rb
+++ W7D1/Rails_Auth_HW/rails_auth/app/models/user.rb
@@ -11,7 +11,7 @@
 #
 
 class User < ApplicationRecord
-    validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: { message: 'Password can\'t be blank'}
   validates :session_token, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
